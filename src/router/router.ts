2cb168c0c9a618fbe6cbada244b1662b9471fe8b
Eric Christensen <hebrew.8.12@gmail.com>
2022-01-13 14:53:32 -0800

    1/13/2022 14:53

--- src/router/router.ts
+++ src/router/router.ts
@@ -3,4 +3,9 @@ import { getGame, getTest } from '../controllers/users.controller';
 import { MyState } from '../main';
 
 export const usersRouter = new Router<MyState>();
-usersRouter.get('/', getGame).get('/test', getTest);
+usersRouter
+  .get('/', getGame)
+  .get('/test', getTest)
+  .get('/api', async (ctx, next) => {
+    ctx.response.body = 'hello!';
+  });
