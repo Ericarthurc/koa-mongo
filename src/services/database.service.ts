435ad2709922a09e2f64eef139ab345f3e0b6e46
Eric Christensen <hebrew.8.12@gmail.com>
2022-01-15 14:35:57 -0800

    1/15/2022 14:35

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,6 +1,33 @@
-import * as mongoDB from 'mongodb';
-import User from '../models/user.model';
-import Service from '../models/service.model';
+import * as mongoDB from "mongodb";
+import Chalk from "chalk";
+
+import User from "../models/user.model";
+import Service from "../models/service.model";
+
+const collectionCreator = async <T>(
+  mongoDb: mongoDB.Db,
+  mongoCollectionList: Pick<mongoDB.CollectionInfo, "name" | "type">[],
+  collectionNameENV: string,
+  collectionOptions?: mongoDB.CreateCollectionOptions | undefined
+): Promise<mongoDB.Collection<T>> => {
+  let collection: mongoDB.Collection<T>;
+  if (
+    mongoCollectionList.find(
+      (collection) => collection.name == collectionNameENV
+    )
+  ) {
+    collection = mongoDb.collection<T>(collectionNameENV);
+    console.log(Chalk.blueBright(`[${collectionNameENV}] collection found!`));
+  } else {
+    collection = await mongoDb.createCollection<T>(
+      collectionNameENV,
+      collectionOptions
+    );
+    console.log(Chalk.greenBright(`Creating ${collectionNameENV} collection!`));
+  }
+
+  return collection;
+};
 
 export const connectToDatabase = async (): Promise<{
   mongoClient: mongoDB.MongoClient;
@@ -15,60 +42,46 @@ export const connectToDatabase = async (): Promise<{
 
   const db: mongoDB.Db = client.db(process.env.MONGO_DB_NAME);
 
-  // let collections: string[] = [];
-  // db.listCollections().toArray((err, items) => {
-  //   if (items != undefined) {
-  //     items.forEach((i) => {
-  //       console.log(i.name);
+  const collectionList = await db
+    .listCollections({}, { nameOnly: true })
+    .toArray();
 
-  //       collections = [...collections, i.name];
-  //     });
-  //   }
-  // });
-
-  // if (
-  //   collections.includes(
-  //     <string>process.env.USERS_COLLECTION_NAME ||
-  //       <string>process.env.SERVICES_COLLECTION_NAME
-  //   )
-  // ) {
-  //   console.log('collections already made!');
-  // }
-
-  const usersCollection: mongoDB.Collection<User> = db.collection(
-    process.env.USERS_COLLECTION_NAME || process.exit(1)
-  );
-
-  // throws error is collection exists... need to check for collections before creation
-  const servicesCollection: mongoDB.Collection<Service> =
-    await db.createCollection(
-      process.env.SERVICES_COLLECTION_NAME || process.exit(1),
-      {
-        validator: {
-          $jsonSchema: {
-            bsonType: 'object',
-            required: ['date', 'seats'],
-            additionalProperties: false,
-            properties: {
-              _id: {},
-              date: {
-                bsonType: 'string',
-                description: "'date' is required and is a string",
-              },
-              seats: {
-                bsonType: 'number',
-                minimum: 0,
-                maximum: 10,
-                description: "'seats' is required and is a number",
-              },
+  const servicesCollection = await collectionCreator<Service>(
+    db,
+    collectionList,
+    <string>process.env.SERVICES_COLLECTION_NAME,
+    {
+      validator: {
+        $jsonSchema: {
+          bsonType: "object",
+          required: ["date", "seats"],
+          additionalProperties: false,
+          properties: {
+            _id: {},
+            date: {
+              bsonType: "string",
+              description: "'date' is required and is a string",
+            },
+            seats: {
+              bsonType: "number",
+              minimum: 0,
+              maximum: 10,
+              description: "'seats' is required and is a number",
             },
           },
         },
-      }
-    );
+      },
+    }
+  );
+
+  const usersCollection = await collectionCreator<User>(
+    db,
+    collectionList,
+    <string>process.env.USERS_COLLECTION_NAME
+  );
 
   console.log(
-    `Successfully connected to database: ${db.databaseName} and collection: ${usersCollection.collectionName}`
+    `Successfully connected to database: ${db.databaseName} and collections: ${usersCollection.collectionName}, ${servicesCollection.collectionName}`
   );
 
   return { mongoClient: client, usersCollection, servicesCollection };
